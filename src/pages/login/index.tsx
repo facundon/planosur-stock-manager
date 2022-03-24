35391e7b359243a54ff7f85ebd27f8aa85c6a319
Facundo Rodriguez <facundonrodriguez@gmail.com>
2022-03-24 11:33:51 -0300

    Fix  login screen

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,34 @@
+import { Center, PinInput, PinInputField, Box, Icon } from "@chakra-ui/react"
 import { useAuth } from "../../auth"
+import { PlanosurLogo } from "../../shared/assets"
 
 const LoginPage: React.FC = () => {
-   const { login, isAuth } = useAuth()
+   const { login } = useAuth()
 
    return (
-      <div>
-         <h1>Is Logged in? {isAuth ? "yes" : "no"}</h1>
-         <button onClick={() => login("admin1234")}>send</button>
-      </div>
+      <>
+         <Center pos="absolute" left={0} right={0} top={20}>
+            <Icon as={PlanosurLogo} color="red.500" w={300} h="auto" />
+         </Center>
+
+         <Center gap={2} h="100vh">
+            <PinInput
+               size="lg"
+               type="alphanumeric"
+               onComplete={value => login(value)}
+               mask
+               manageFocus
+               otp
+            >
+               <PinInputField />
+               <PinInputField />
+               <PinInputField />
+               <PinInputField />
+               <PinInputField />
+               <PinInputField />
+            </PinInput>
+         </Center>
+      </>
    )
 }
 
