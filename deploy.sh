1dbbf4876575af79333e85a40a35607080d862d1
Facundo Rodriguez <facundonrodriguez@gmail.com>
2022-03-24 21:33:37 -0300

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b master
+git add -A
+git commit -m 'deploy'
+
+git push -f git@me.github.com:facundon/planosur-stock-manager-page.git master:gh-pages
+
+cd -
